d6ab468fb54001fe74345e9f95a398a5bea8db87
Anthony Mapes, Jr <anthonymapes3@gmail.com>
2017-08-15 21:21:12 -0400

    Probably does the basics

--- shasplitter.py
+++ shasplitter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import argparse
+import os
 
 # Handle the CLI arguments
 parser = argparse.ArgumentParser(description="Split a large file into smaller files")
@@ -8,7 +9,8 @@ parser.add_argument("in_file",
 	help="The input file to read.  Must be in Zefania XML format.",
 	nargs=1)
 parser.add_argument("-o", "--outdir",
-	help="The directory to store the output files in.",
+	help="The directory to store the output files in.\n"
+		+ "Note that the filenames will be all UPPERCASE",
 	dest="out_dir",
 	default="out/")
 parser.add_argument("-n", "--names",
@@ -17,3 +19,24 @@ parser.add_argument("-n", "--names",
 	dest='num_chars',
 	default=2)
 results = parser.parse_args()
+
+# This dict will contain references to each open file.
+# Key will be the first X chars, value will be ref to OPENED file.
+outfiles = dict()
+
+# Open the file and read it one line at a time
+with open(results.in_file) as input_file:
+	for line in input_file:
+		outfname = line[:results.num_chars].upper() # out file name (minus .txt)
+		if outfname not in outfiles.keys():
+			# Open the file for appending and put it in the dict
+			outfiles[outfname] = open(out_dir + outfname + ".txt", 'a')
+		outfiles[outfname].write(line)
+
+# Close all output files
+for f in outfiles:
+	print("Closing " + f.name + "... ", end='')
+	f.flush()
+	os.fsync(f.fileno())
+	f.close()
+	print("Done!")
